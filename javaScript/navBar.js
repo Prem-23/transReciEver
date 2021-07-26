7edd319ecfd42dc7f08280069527b2658119338e
Premkumar <71540670+Prem-23@users.noreply.github.com>
2021-07-26 13:20:43 +0530

    Update navBar.js

--- javaScript/navBar.js
+++ javaScript/navBar.js
@@ -1,48 +1,41 @@
 const Toggle = document.getElementById('toggle');
 const logo = document.getElementById('header');
-const add = document.getElementById('add');
-const del = document.getElementById('del');
+const add = document.getElementById('addChats');
 const navSelect = document.getElementById('headerToggle');
 const searchBar = document.querySelector('.searchInput');
 const searchIcon = document.querySelector('.searchIcon');
-const section = document.querySelector('.sectionOne');
-const Hide = document.querySelector('.containers');
+const section = document.querySelector('.sectionContainer');
+const container = document.querySelector('.chatAndMsgSectionContainer');
 
 Toggle.addEventListener('click', function(){
     Toggle.classList.toggle('active');
     navSelect.classList.toggle('navActive');
-    Hide.classList.toggle('hideOne')
-});
-
-del.addEventListener('click', function(){
-    Toggle.classList.remove('active');
-    navSelect.classList.remove('navActive');
-    Hide.classList.remove('hideOne')
+    container.classList.toggle('container');
 });
 add.addEventListener('click', function(){
     Toggle.classList.remove('active');
     navSelect.classList.remove('navActive');
-    Hide.classList.remove('hideOne')
+    container.classList.remove('container');
 });
 logo.addEventListener('click', function(){
     Toggle.classList.remove('active');
     navSelect.classList.remove('navActive');
-    Hide.classList.remove('hideOne')
+    container.classList.remove('container');
 });
 searchBar.addEventListener('click', function(){
     Toggle.classList.remove('active');
     navSelect.classList.remove('navActive');
-    Hide.classList.remove('hideOne')
+    container.classList.remove('container');
 });
 searchIcon.addEventListener('click', function(){
     Toggle.classList.remove('active');
     navSelect.classList.remove('navActive');
-    Hide.classList.remove('hideOne')
+    container.classList.remove('container');
 });
 section.addEventListener('click', function(){
     Toggle.classList.remove('active');
     navSelect.classList.remove('navActive');
-    Hide.classList.remove('hideOne')
+    container.classList.remove('container');
 });
 
 
